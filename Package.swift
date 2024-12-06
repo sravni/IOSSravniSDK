baec2b6333f221b9684054f6d95a91848d95c7d4
mr.kutsin <mr.kutsin@gmail.com>
2024-12-06 05:35:02 -0600

    Update checksum for SravniSDK.xcframework.zip

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SravniSDK",
-            url: "https://github.com/sravni/IOSSravniSDK/releases/download/1.0.25/SravniSDK.xcframework.zip",
-            checksum: "bbe2e58ac8dbea9c9e7e5cb98f01fbbe08e82c8cc52840b869f1e4b3cb1f67e0"
+            url: "https://github.com/sravni/IOSSravniSDK/releases/download/1.0.26/SravniSDK.xcframework.zip",
+            checksum: "51fcf3e49f1c084000a47b6c0002a171d73dd450ddeda4cf24752d6560e8f869"
         )
     ]
 )
