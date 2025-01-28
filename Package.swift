ea918175ef03a8dc0f053a70d617aeb242d49cf1
Daniil Kravchuk <daniil.kravchuk@sravni.team>
2025-01-28 13:21:38 +0300

    Update checksum & version & podspec for SravniSDK.xcframework.zip

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SravniSDK",
-            url: "https://github.com/sravni/IOSSravniSDK/releases/download/1.0.177/SravniSDK.xcframework.zip",
-            checksum: "eb72f438d458ecfffb9c3c4970f4530afd5d28a5263f733f848269aa31a6cae9"
+            url: "https://github.com/sravni/IOSSravniSDK/releases/download/1.0.178/SravniSDK.xcframework.zip",
+            checksum: "0f81ce4abc7e17ed88d4f18f67f632c863f7307f0d922a95b1cae907b2399ee4"
         )
     ]
 )
