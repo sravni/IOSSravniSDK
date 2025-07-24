fdd3727898b82faa91f0b382edcb68b737b658e4
DenisLarionov <denis.larionov@sravni.net>
2025-07-24 16:13:49 +0300

    bump minimal iOS version to 16

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SravniSDK",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         .library(name: "SravniSDK", targets: ["SravniSDK"])
